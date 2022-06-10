7f6247dde2709f2578327a912f4f8f0ef748aec7
TahiryMariano <tahirymariano.htm@gmail.com>
2022-06-10 08:55:18 +0200

    Ajout des derniers fichiers

---
+++ config/grid.config.js
@@ -0,0 +1,11 @@
+
+export default {
+    settings: {
+        gridSelector: '#grid',
+        svgSelector: '#gridSvg',
+        cellSize: 20,
+        borderSize: 1,
+        borderColor: 'white',
+        verticesWeight: 10,
+    }
+}
